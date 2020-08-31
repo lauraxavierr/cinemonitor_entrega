a229bef719737bf46aec13933930254facdaed95
Laura Xavier Pereira <lauaraxavierpereira1@gmail.com>
2020-08-31 02:22:25 +0000

    Finalizando atividade

---
+++ Filme.java
@@ -0,0 +1,29 @@
+class filmes {
+  private String titulo;
+  private int ano, tipo;
+
+public void setTitulo(String tit) {
+  titulo = tit;
+}
+
+public void setAno(int an) {
+    ano = an;
+}
+
+public void setTipo(int tip) {
+    tipo = tip;
+}
+
+public String getTitulo() {
+    return titulo;
+}
+
+public int getAno() {
+    return ano;
+}
+
+public int getTipo() {
+    return tipo;
+}
+
+  }
